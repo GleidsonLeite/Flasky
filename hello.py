01ba4efcefb02931dff5432f2c12bdf0bdb7da2f
Gleidson Leite <gledson.leytte@hotmail.com>
2020-10-02 14:59:59 -0300

    Adding jinja2 template

--- hello.py
+++ hello.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
+
+# extensions
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
+    return render_template(
+        'index.html',
+        current_time=datetime.utcnow()
+    )
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hi, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+Bootstrap(app)
+Moment(app)
